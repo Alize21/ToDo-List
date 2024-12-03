99d82e86153f27ce5467083ae47393a2268543a1
Alize21 <pusikentut@gmail.com>
2024-12-03 19:51:21 +0700

    adding mark button for complete task

--- script.js
+++ script.js
@@ -10,13 +10,20 @@ button.addEventListener("click", function () {
   }
 
   const taskContainer = document.createElement("li");
-  const task = document.createTextNode(input.value);
+  const task = document.createElement("p");
+  task.textContent = input.value;
+
   taskContainer.appendChild(task);
 
   const hapusButton = document.createElement("button");
   const hapusTeks = document.createTextNode("Hapus task");
   hapusButton.appendChild(hapusTeks);
   taskContainer.appendChild(hapusButton);
+
+  const completeButton = document.createElement("button");
+  completeButton.textContent = "Tandai sebagai selesai";
+  completeButton.classList.add("completed-button");
+  taskContainer.appendChild(completeButton);
 
   taskContainer.classList.add("task");
   hapusButton.classList.add("hapus-task");
@@ -29,5 +36,10 @@ myTasks.addEventListener("click", function (e) {
   if (e.target.classList.contains("hapus-task")) {
     const taskToRemove = e.target.parentElement;
     myTasks.removeChild(taskToRemove);
+  }
+
+  if (e.target.classList.contains("completed-button")) {
+    const taskToMark = e.target.parentElement;
+    taskToMark.classList.toggle("completed");
   }
 });
